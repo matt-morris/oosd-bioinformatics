a2ff4a25739a08c0b7aa9ca345ae674f2e35aaa6
griffin <griffin@localhost>
2011-04-07 16:21:14 -0600

    Testing, testing. 1, 2, 3.

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Apr 7, 2011
+ *      Author: griffin
+ */
+
+#include <iostream>
+using namespace std;
+
+int main ( )
+{
+	cout << "Hello, world!";
+	return 0;
+}
